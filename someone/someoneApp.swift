484b0c01831d84423f7995e03b388639025a5adb
Edward <edwardguil96@outlook.com>
2021-09-10 18:46:53 +1000

    Initial Commit

---
+++ someone/someoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  someoneApp.swift
+//  someone
+//
+//  Created by Christina Riabokon on 10/9/21.
+//
+
+import SwiftUI
+
+@main
+struct someoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
